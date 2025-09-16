5933a351c512e8b45919a03bc5f2b2ebc9647662
Tolik Tserunyan <toliktserunyan@MacBook-Air-Tolik.local>
2025-09-16 11:54:55 +0400

    Added tags for and if

--- women/views.py
+++ women/views.py
@@ -3,11 +3,20 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 
 
+data_db = [
+    {'name': 'Tolik', 'surname': 'Tserunyan', 'age': 29, 'is_published': False},
+    {'name': 'Hasmik', 'surname': 'Mirzoyan', 'age': 26, 'is_published': True},
+    {'name': 'Gago', 'surname': 'Harutyunyan', 'age': 30, 'is_published': True},
+    {'name': 'Tatev', 'surname': 'Chaparyan', 'age': 23, 'is_published': True}
+]
+
+
 def index(request):
     # t = render_to_string('index.html')
     # return HttpResponse(t)
     data = {
-        'title': 'Main page from the data'
+        'title': 'Main page from the data',
+        'users': data_db
     }
     return render(request, 'index.html', data)
 
